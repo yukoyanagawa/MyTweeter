afd9e9a9b213dbeac8ad5ed72e075f2d8eb291db
NewsPics01 <yuko.yanagawa@uzabase.com>
2015-06-11 19:24:58 +0900

    Add Image and Menu

--- app/src/main/java/com/example/newspicks01/mytweeter/MainActivity.java
+++ app/src/main/java/com/example/newspicks01/mytweeter/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.newspicks01.mytweeter;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.loopj.android.image.SmartImageView;
+
 import java.util.List;
 
-import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
@@ -41,16 +46,11 @@ public class MainActivity extends ListActivity {
     }
 
     private void reloadTimeLine() {
-        AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>() {
+        AsyncTask<Void, Void, List<twitter4j.Status>> task = new AsyncTask<Void, Void, List<twitter4j.Status>>() {
             @Override
-            protected List<String> doInBackground(Void... params) {
+            protected List<twitter4j.Status> doInBackground(Void... params) {
                 try {
-                    ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
-                    ArrayList<String> list = new ArrayList<String>();
-                    for (twitter4j.Status status : timeline) {
-                        list.add(status.getText());
-                    }
-                    return list;
+                    return mTwitter.getHomeTimeline();
                 } catch (TwitterException e) {
                     e.printStackTrace();
                 }
@@ -58,10 +58,10 @@ public class MainActivity extends ListActivity {
             }
 
             @Override
-            protected void onPostExecute(List<String> result) {
+            protected void onPostExecute(List<twitter4j.Status> result) {
                 if (result != null) {
                     mAdapter.clear();
-                    for (String status : result) {
+                    for (twitter4j.Status status : result) {
                         mAdapter.add(status);
                     }
                     getListView().setSelection(0);
@@ -77,10 +77,30 @@ public class MainActivity extends ListActivity {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
-    private class TweetAdapter extends ArrayAdapter<String> {
+    private class TweetAdapter extends ArrayAdapter<twitter4j.Status> {
+
+        private LayoutInflater mInflater;
 
         public TweetAdapter(Context context) {
             super(context, android.R.layout.simple_list_item_1);
+            mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.list_item_tweet, null);
+            }
+            Status item = getItem(position);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getUser().getName());
+            TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+            screenName.setText("@" + item.getUser().getScreenName());
+            TextView text = (TextView) convertView.findViewById(R.id.text);
+            text.setText(item.getText());
+            SmartImageView icon=(SmartImageView)convertView.findViewById(R.id.icon);
+            icon.setImageUrl(item.getUser().getProfileImageURL());
+            return convertView;
         }
     }
 
@@ -96,13 +116,11 @@ public class MainActivity extends ListActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                reloadTimeLine();
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
