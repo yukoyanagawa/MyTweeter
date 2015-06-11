11db6c195fb7fb46b0fe5e1860bd9761ef1ee4c1
NewsPics01 <yuko.yanagawa@uzabase.com>
2015-06-11 19:50:28 +0900

    addTweetFiled

---
+++ app/src/main/java/com/example/newspicks01/mytweeter/TweetActivity.java
@@ -0,0 +1,17 @@
+package com.example.newspicks01.mytweeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by newspicks01 on 15/06/11.
+ */
+public class TweetActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+    }
+
+}
